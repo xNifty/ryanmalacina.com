6c3d8cf4c4952dff90cbf3f989129839c8b52a4b
xNifty <ninja@snoring.ninja>
2018-02-05 20:55:37 -0500

    moved assets folder

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var path = require('path');
 
 var app = express();
 
 app.engine('handlebars', exphbs({}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('assets'))
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get("/", function(req, res) {
    res.render("index");
