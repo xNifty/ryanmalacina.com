3926ff4465c8bb857a7c4f97d0768c083f67677d
xNifty <ninja@snoring.ninja>
2018-02-08 19:44:10 +0000

    Add error handling

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var exphbs  = require('express-handlebars');
 var path = require('path');
 
 var app = express();
+var env = app.settings.env;
 
 app.engine('handlebars', exphbs({}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(__dirname + '/views'));
 
 // global, passed to all views that have the footer
 var currentyear = new Date().getFullYear();
@@ -30,5 +31,45 @@ app.get("/contact", function(req, res) {
    });
 });
 
+// Redirect to blog
+app.get("/blog", function(req, res) {
+  res.redirect(301, "https://blog.endgame.wtf");
+});
+
+// catch 404 and forward to error handler
+// note this is after all good routes and is not an error handler
+// to get a 404, it has to fall through to this route - no error involved
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers - these take err object.
+// these are per request error handlers.  They have two so in dev
+// you get a full stack trace.  In prod, first is never setup
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 app.listen(8080);
-console.log("Server is now running.");
+console.log("Server is now running in " + env + " mode.");
