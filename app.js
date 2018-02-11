52a2200e73fed28fe5818c80ef033ce513e11900
xNifty <ninja@snoring.ninja>
2018-02-10 23:33:37 -0500

    Don't pass error var to prod render

--- app.js
+++ app.js
@@ -73,8 +73,7 @@ if (app.get('env') === 'production') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
-            message: err.message,
-            error: {}
+            message: err.message
         });
     });
 }
