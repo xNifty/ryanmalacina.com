efc4e68949331a6b06dc9826ba36dfc60467e20f
xNifty <ninja@snoring.ninja>
2018-02-28 19:49:15 +0000

    Add back blog redirect

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ app.get("/contact", function(req, res) {
    });
 });
 
+app.get("/blog", function(req, res) {
+   res.redirect(301, "https://blog.endgame.wtf");
+});
+
 // catch 404 and forward to error handler
 // note this is after all good routes and is not an error handler
 // to get a 404, it has to fall through to this route - no error involved
