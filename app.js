f28ceb72c9bf052baf7eb66f72a287aab6b7a9ed
Ryan M <ryan@snoring.ninja>
2018-03-01 16:30:18 -0500

    Add docs route redirect

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.get("/blog", function(req, res) {
    res.redirect(301, "https://blog.endgame.wtf");
 });
 
+app.get("/docs", function(req, res) {
+   res.redirect(301, "https://docs.endgame.wtf");
+});
+
 // catch 404 and forward to error handler
 // note this is after all good routes and is not an error handler
 // to get a 404, it has to fall through to this route - no error involved
