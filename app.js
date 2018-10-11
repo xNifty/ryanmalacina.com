1a5fb793e572a2a1819ff0974c1802fa1e3a1e68
Ryan Malacina <ryan@ryanmalacina.com>
2018-10-11 08:42:52 -0400

    Change saveUninitialized to false

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use(bodyParser.urlencoded(
 let sess = {
     secret: config.get('rmPrivateKey'),
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
     name: 'safjhkashfjkasjkfhjkashfjhaskdfjhhsad',
     cookie: { secure: false, httpOnly: true, maxAge: 24 * 60 * 60 * 1000, },
     store: new MongoStore({ mongooseConnection: mongoose.connection, clear_interval: 3600  })
