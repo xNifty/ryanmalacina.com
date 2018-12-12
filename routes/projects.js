e5224a354f6056ed82d7f3a7d3a18a870b1739d6
xNifty <ryan@ryanmalacina.com>
2018-12-11 22:37:20 -0500

    Fix edit/new, image uploads and more Added file uploading to both new projects and when editing projects. Locally, updated NPM so this yarn file includes the install of body-parser as well.  There is a lot of clean up that needs to be done, as there is a lot of assumption based logic within.  Most of said assumptions should be correct, but shouldn't be relying on that to always be the case.

--- routes/projects.js
+++ routes/projects.js
@@ -10,8 +10,11 @@ const session = require('express-session');
 const showdown = require('showdown');
 const sanitize = require('sanitize-html');
 const dateformat = require('dateformat');
+const fileUpload = require('express-fileupload');
 
 let converter = new showdown.Converter();
+
+router.use(fileUpload());
 
 router.get("/", async (req, res) => {
     let project_list = await listProjects();
@@ -46,6 +49,7 @@ router.get('/new', [auth], async(req, res) => {
 
 router.post('/new', auth, async(req, res) => {
     const { error } = validate(req.body);
+
     if (error) return res.status(400).render('new-project', {
         layout: 'new-project',
         new_project: true,
@@ -54,22 +58,26 @@ router.post('/new', auth, async(req, res) => {
         project_title: req.body.project_title,
         project_source: req.body.project_source,
         project_description: req.body.project_description,
-        project_image: req.body.project_image
+        project_image: req.files.project_image.name
     });
 
     let project = new Project(_.pick(req.body, [
-        'project_name', 'project_title', 'project_source', 'project_image'
+        'project_name', 'project_title', 'project_source'
     ]));
 
     let pDescription = converter.makeHtml(req.body.project_description);
     let pSanitized = sanitize(pDescription, { allowedTags: sanitize.defaults.allowedTags.concat(['h1']) });
+    let pImage = req.files.project_image;
     project.project_description_markdown = req.body.project_description;
     project.project_description_html = pSanitized;
+    project.project_image = pImage.name; // File name, nothing else
     let saveDate = new Date(Date.now());
 
     try {
+        await pImage.mv('./public/images/' + pImage.name);
         await project.save();
     } catch(ex) {
+        console.log(ex);
         return res.status(400).render('new-project', {
             layout: 'new-project',
             new_project: true,
@@ -78,8 +86,7 @@ router.post('/new', auth, async(req, res) => {
             project_title: req.body.project_title,
             project_source: req.body.project_source,
             project_description: req.body.project_description,
-            project_image: req.body.project_image,
-	    last_edited: saveDate
+	        last_edited: saveDate
         });
     }
     req.session.success = 1;
@@ -94,53 +101,138 @@ router.get('/:name/edit', [auth], async(req, res) => {
 
     req.session.project_id = project._id;
 
+    // For if the edit fails, otherwise we want to return to the normal project information page
+    req.session.projectEditReturnTo = req.originalUrl;
+
     let error_message = req.session.error_message;
     req.session.error_message = null;
 
-    res.render('new-project', {
-        layout: 'new-project',
-        update_project: true,
-        project_name: project.project_name,
-        project_title: project.project_title,
-        project_source: project.project_source,
-        project_description: project.project_description_markdown,
-        project_image: project.project_image,
-        error_message: error_message
-    });
+    // If not load from session, then load normally; else, load from session
+    if (!req.session.loadProjectFromSession) {
+        res.render('new-project', {
+            layout: 'new-project',
+            update_project: true,
+            project_name: project.project_name,
+            project_title: project.project_title,
+            project_source: project.project_source,
+            project_description: project.project_description_markdown,
+            project_image: project.project_image,
+            error_message: error_message
+        });
+    } else {
+        res.render('new-project', {
+            layout: 'new-project',
+            update_project: true,
+            project_name: req.session.project_name,
+            project_title: req.session.project_title,
+            project_source: req.session.project_source,
+            project_description: req.session.project_description_markdown,
+            project_image: req.session.project_image,
+            error_message: error_message
+        });
+        delete req.session.loadProjectFromSession;
+        delete req.session.project_name;
+        delete req.session.project_title;
+        delete req.session.project_source;
+        delete req.session.project_description_markdown
+        delete req.session.project_image;
+    }
 });
 
 router.post('/edit', auth, async(req, res) => {
-    try {
-	let pDescription = converter.makeHtml(req.body.project_description);
-	let pSanitized = sanitize(pDescription, { allowedTags: sanitize.defaults.allowedTags.concat(['h1']) });
-	let saveDate = new Date(Date.now());
+    let currentImage = await Project.find({_id: req.session.project_id}).select(
+        { project_image: 1, _id: 0 });
 
-        let project = await Project.findByIdAndUpdate({_id: req.session.project_id}, {
-            project_name: req.body.project_name,
-            project_title: req.body.project_title,
-            project_source: req.body.project_source,
-            project_description_markdown: req.body.project_description,
-            project_description_html: pSanitized,
-            project_image: req.body.project_image,
-	    last_edited: saveDate
-        });
+    try {
+        const { error } = validate(req.body);
+        if (error) {
+            for (let i = 0; i < error.details.length; i++) {
+                console.log(error.details[i].context.key);
+                if (error.details[i].context.key === 'project_description')
+                    throw new Error("The project description must be at least 20 characters in length.");
+            }
+        }
+
+        let pDescription = converter.makeHtml(req.body.project_description);
+
+        // We want to allow the h1 tag in our sanitizing
+        let pSanitized = sanitize(pDescription, { allowedTags: sanitize.defaults.allowedTags.concat(['h1']) });
+        let saveDate = new Date(Date.now());
+        let pImage = '';
+
+        // @TODO : CLEAN ALL OF THIS UP; THIS IS UGLY
+        try {
+            pImage = req.files.project_image;
+        } catch (ex) {
+            pImage = '';
+        }
+
+        // If no image is selected, set it to the existing image; otherwise, error because we need an image
+        if (!pImage) {
+            if (currentImage[0].project_image) {
+                pImage = currentImage[0].project_image;
+                await Project.findByIdAndUpdate({_id: req.session.project_id}, {
+                    project_name: req.body.project_name,
+                    project_title: req.body.project_title,
+                    project_source: req.body.project_source,
+                    project_description_markdown: req.body.project_description,
+                    project_description_html: pSanitized,
+                    project_image: pImage,
+                    last_edited: saveDate
+                });
+            }
+            else {
+                throw new Error("You must upload an image.")
+            }
+        } else {
+            // Only move if the image has a different name; unique names only, not that hard
+            // @TODO : really should limit file sizes to the ideal image size of 263x263
+            if (pImage.name === currentImage[0].project_image) { // Boy, this is just dumb
+                throw new Error("Please make sure file names are unique.  If you've already uploaded " +
+                    "this image, you do not need to upload it again.");
+            } else {
+                // Not the same, so move it!
+                // This condition is only ever going to be met if it's a new image, but maybe should add some security
+                // to this later on, just in case
+                await pImage.mv('./public/images/' + pImage.name);
+                await Project.findByIdAndUpdate({_id: req.session.project_id}, {
+                    project_name: req.body.project_name,
+                    project_title: req.body.project_title,
+                    project_source: req.body.project_source,
+                    project_description_markdown: req.body.project_description,
+                    project_description_html: pSanitized,
+                    project_image: pImage.name,
+                    last_edited: saveDate
+                });
+            }
+        }
+
         req.session.success = 1;
-        req.session.success_message = "Project edited successfully";
+        req.session.success_message = "Project edited successfully!";
         req.session.project_id = null;
 
-	if (req.session.projectReturnTo) {
-            var returnTo = req.session.projectReturnTo;
-	    delete req.session.projectReturnTo;
+        if (req.session.projectReturnTo) {
+            let returnTo = req.session.projectReturnTo;
+            delete req.session.projectReturnTo;
+            delete req.session.projectEditReturnTo; // Still need to delete even though we didn't use it
             return res.redirect(returnTo);
         } else {
             return res.redirect('/projects');
         }
     } catch(ex) {
-        req.session.error_message = 'Something went wrong; please try again.';
+        // @TODO: So this is hacky and should be redone later
+        req.session.loadProjectFromSession = true;
+        req.session.project_name = req.body.project_name;
+        req.session.project_title = req.body.project_title;
+        req.session.project_source = req.body.project_source;
+        req.session.project_description_markdown = req.body.project_description;
+        req.session.project_image = currentImage[0].project_image;
+
+        req.session.error_message = ex.message;
         if (req.session.projectReturnTo) {
-            var returnTo = req.session.projectReturnTo;
-            delete req.session.projectReturnTo;
-            return res.redirect(returnTo);
+            let returnToEdit = req.session.projectEditReturnTo;
+            delete req.session.projectEditReturnTo;
+            return res.redirect(returnToEdit);
         } else {
             return res.redirect('/projects');
         }
@@ -150,7 +242,7 @@ router.post('/edit', auth, async(req, res) => {
 });
 
 // TODO: this really should use ID to load; we can hide that on the page per row if we load initial
-// project listing from the database
+// We can clearly get the ID like we do in other routes, so this really needs to be changed to ID loading
 router.get("/:name", async(req, res) => {
    const project = await Project.findOne({
        project_name: req.params.name
