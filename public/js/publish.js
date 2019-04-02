c867003b9d597f70314be8ae340761e75c7815ba
xNifty <ryan@ryanmalacina.com>
2019-04-02 13:04:13 -0400

    Okay, really broke it.

--- public/js/publish.js
+++ public/js/publish.js
@@ -3,21 +3,24 @@ $(document).ready(function() {
     $('#adminprojectlist').load('/admin/projects/api/get');
 });
 
-$(document).on("click", ".publish", function() {
-    publishProject();
-    $('#adminprojectlist').load('/admin/projects/api/get');
+$(function(){
+    $('.publish').click(function (evt) {
+        publishProject();
+    });
 });
 
-$(document).on("click", ".unpublish", function() {
-    unpublishProject();
-    $('#adminprojectlist').load('/admin/projects/api/get');
+$(function(){
+    $('.unpublish').click(function (evt) {
+        unpublishProject();
+    });
 });
 
 function publishProject() {
-    alert("CLICKED");
+    let id = $(this).data("id");
+    alert($(this).data("id"));
     $.ajax({
         type:'PUT',
-        url: '/admin/projects/api/publish/'+ $(this).data('id'),
+        url: '/admin/projects/api/publish/'+ $(this).data('data-id'),
     }).done(function(res) {
         if (res.success) {
             // reload page
@@ -29,10 +32,10 @@ function publishProject() {
 }
 
 function unpublishProject() {
-    alert("CLICKED");
+    alert($(this).data('id'));
     $.ajax({
         type:'PUT',
-        url: '/admin/projects/api/unpublish/'+ $(this).data('id'),
+        url: '/admin/projects/api/unpublish/'+ $(this).data('data-id'),
     }).done(function(res) {
         if (res.success) {
             // reload page
