e7a543e6187b5877ec67c1507d4ac39e2c1bce13
xNifty <ryan@ryanmalacina.com>
2019-04-19 13:01:38 -0400

    Add nonworking contact form, anchor tags

--- public/js/site.js
+++ public/js/site.js
@@ -36,17 +36,88 @@ function unpublishProject() {
 }
 
 $(document).ready(function(){
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#scroll').fadeIn();
         } else {
             $('#scroll').fadeOut();
         }
     });
-    $('#scroll').click(function(){
+    $('#scroll').click(function() {
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
+    $('#submitmail').click(function() {
+        validateForm;
+    });
 });
 
-$("#homepage").hide().show("slide", { direction: "left" }, 1500);
+function validateForm() {
+    let name =  document.getElementById('name').value;
+    console.log(name);
+    if (name === "") {
+        document.getElementById('status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    let email =  document.getElementById('email').value;
+    if (email === "") {
+        document.getElementById('status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.getElementById('status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    let subject =  document.getElementById('subject').value;
+    if (subject === "") {
+        document.getElementById('status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    let message =  document.getElementById('message').value;
+    if (message === "") {
+        document.getElementById('status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    document.getElementById('status').innerHTML = "Sending...";
+    document.getElementById('contact-form').submit();
+
+}
+
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+            &&
+            location.hostname === this.hostname
+        ) {
+            // Figure out element to scroll to
+            let target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    let $target = $(target);
+                    //$target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        //$target.focus(); // Set focus again
+                    }
+                });
+            }
+        }
+    });
