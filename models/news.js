33a0822b74f649852aeba1f987a78e359efe6b01
xNifty <ryan@ryanmalacina.com>
2019-09-25 12:50:56 -0400

    Remove custom env file, joi to @hapi/joi

--- models/news.js
+++ models/news.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 
 const newsScheme = new mongoose.Schema({
     news_title: {
--- models/projects.js
+++ models/projects.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 
 const projectScheme = new mongoose.Schema({
     project_name: {
