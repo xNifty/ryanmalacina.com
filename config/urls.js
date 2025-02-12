d446a3b4fe95f1fa99f16c46331475c2a64dc34d
xNifty <ryan@ryanmalacina.com>
2025-02-12 06:08:11 -0500

    content-src for recaptcha

--- config/urls.js
+++ config/urls.js
@@ -16,7 +16,7 @@ const styleSrc = [
   `https://cdn.jsdelivr.net`,
 ];
 const imgSrc = [`https://i.imgur.com`, `*.s3.amazonaws.com`, `https://github.githubassets.com`, `https://github.com/user-attachments/`];
-const connectSrc = [`https://cdn.jsdelivr.net`];
+const connectSrc = [`https://cdn.jsdelivr.net`, `https://www.google.com/recaptcha/`];
 const fontSrc = [
   `https://cdnjs.cloudflare.com`,
   `https://fonts.gstatic.com`,
